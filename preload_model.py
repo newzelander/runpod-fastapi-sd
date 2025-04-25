5f807cce4265211dd780027b2d3e10834cd1eb31
newzelander <tarviky@gmail.com>
2025-04-25 18:33:04 +0300

    Update preload_model.py

--- preload_model.py
+++ preload_model.py
@@ -2,6 +2,7 @@ from diffusers import StableDiffusion3Pipeline
 
 # Pre-download and cache the model (this will only happen once)
 StableDiffusion3Pipeline.from_pretrained(
-    stabilityaistable-diffusion-3.5-large, 
-    cache_dir=workspacemodelsstable-diffusion-3.5
+    "stabilityai/stable-diffusion-3.5",
+    cache_dir="/workspace/models/stable-diffusion-3.5"
 )
+
