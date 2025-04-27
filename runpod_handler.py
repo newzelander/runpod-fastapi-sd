39bbec04f314dab85c994e780a4ca6b5096181dd
newzelander <tarviky@gmail.com>
2025-04-27 15:02:05 +0300

    Update runpod_handler.py

--- runpod_handler.py
+++ runpod_handler.py
@@ -1,12 +1,20 @@
-import os
 import runpod
-
-MODEL_PATH = "/runpod-volume/models/stable-diffusion-3.5-large"
+from main import generate_image
+import time
 
 def my_handler(event):
-    if os.path.exists(MODEL_PATH):
-        return {"status": "success", "message": "Model folder exists."}
-    else:
-        return {"status": "error", "message": "Model path not found."}
+    prompt = event["input"]["prompt"]
+    seed = event["input"].get("seed", None)
+
+    start_time = time.time()
+    filename = generate_image(prompt, seed)
+    end_time = time.time()
+
+    return {
+        "output": {
+            "image_url": f"https://{event['runpod']['endpoint']}/output/{filename}",
+            "time_taken_seconds": round(end_time - start_time, 2)
+        }
+    }
 
 runpod.serverless.start({"handler": my_handler})
