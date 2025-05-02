3774a4bebf4d7af91140375ac60ab89f671f4138
newzelander <tarviky@gmail.com>
2025-05-03 01:28:43 +0300

    Update preload_model.py

--- preload_model.py
+++ preload_model.py
@@ -5,7 +5,7 @@ def download_model(model_path):
     # If the model does not exist in the target path, download it
     if not os.path.exists(model_path):
         snapshot_download(
-            repo_id="stabilityai/stable-diffusion-3.5-large",
+            repo_id="stabilityai/stable-diffusion-3.5-large",  # Correct repo_id for the model
             local_dir=model_path,
             local_dir_use_symlinks=False,
             resume_download=True
