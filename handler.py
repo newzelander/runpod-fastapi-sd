2af92bdf387fa2f18dc0da2e92f6b72779f1e791
newzelander <tarviky@gmail.com>
2025-05-03 23:29:32 +0300

    Delete handler.py

--- handler.py
+++
@@ -1,8 +0,0 @@
-from preload_model import get_or_download_model
-
-def handler(event):
-    model_path = get_or_download_model()
-    return {
-        "status": "ready",
-        "model_path": model_path
-    }
